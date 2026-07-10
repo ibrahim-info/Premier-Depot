0c09b42cb3a935badcc1f873edcb2e16daf5ebe7
Barry <baarrypremier1e@gmail.com>
2026-07-10 18:59:45 +0200

    commit 2

---
+++ fic1.cpp
@@ -0,0 +1,7 @@
+#include<iostream>
+#include<string>
+using namespace std;
+int main(){
+	cout << " cest un simple teste de depot sur le nouveau git de travail il y a encore beaucoup de chose a faire dans cette affaire";
+	return 0;
+	}
